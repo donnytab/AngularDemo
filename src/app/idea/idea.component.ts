fff401eaaed456942269997cb61916d19e58608c
downson <downson.yuen@gmail.com>
2018-10-18 23:59:48 -0400

    Create idea object

--- src/app/idea/idea.component.ts
+++ src/app/idea/idea.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Idea} from '../idea';
 
 @Component({
   selector: 'app-idea',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./idea.component.css']
 })
 export class IdeaComponent implements OnInit {
-  ideaItem = 'Arsene Wenger';
+  ideaItem : Idea = {
+    id: 1;
+    keyword: 'Arsene Wenger';
+    note: 'Former Arsenal FC manager(1996-2018) and club legend';
   constructor() { }
 
   ngOnInit() {
