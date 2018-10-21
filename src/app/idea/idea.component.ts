47abecd0936c169a8b51209264bcdf32af9e743d
downson <downson.yuen@gmail.com>
2018-10-20 23:58:34 -0400

    Add idea properties

--- src/app/idea/idea.component.ts
+++ src/app/idea/idea.component.ts
@@ -8,9 +8,10 @@ import {Idea} from '../idea';
 })
 export class IdeaComponent implements OnInit {
   ideaItem : Idea = {
-    id: 1;
-    keyword: 'Arsene Wenger';
-    note: 'Former Arsenal FC manager(1996-2018) and club legend';
+    id: 1,
+    keyword: 'Arsene Wenger',
+    note: 'Former Arsenal FC manager(1996-2018) and club legend'
+  };
   constructor() { }
 
   ngOnInit() {
